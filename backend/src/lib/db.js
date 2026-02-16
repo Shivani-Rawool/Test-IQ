eeba4ce0831bcfd5e090fa931fc505cd906e75ec
Shivani-Rawool <Shivanirawool017@gmail.com>
2026-02-16 12:26:04 +0530

    Validation check for DB env variables

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ import {ENV} from "./env.js";
 export const connectDB = async() =>{
     try {
         if(!ENV.DB_URL){
-            throw new error("DB_URL not found in env variables")
+            throw new error("DB_URL not found in environment variables")
         }
         const conn = await mongoose.connect(ENV.DB_URL)
         console.log("Connected to MongoDB", conn.connection.host) 
